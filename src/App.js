c24e7d0a220517861670f13141eb48b6432243a4
Guruprasad <guruprasadsr10@gmail.com>
2021-08-08 17:41:27 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkform from './components/ImageLinkform/ImageLinkfrom';
+import Rank from './components/Rank/Rank'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(){
+		super();
+		this.state={
+			input:'',
+		}
+	}
+	onInputchange=(event)=>{
+		console.log(event.target.value); //to get input value in console
+	}
+	onSubmit=()=>{
+		console.log("Click");
+		
+	}
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+    params={{
+	    "particles": {
+	        "number": {
+	            "value": 250,
+	            "density": {
+	                "enable": true,
+                  "value_area":800
+	            }
+	        },
+	        "size": {
+	            "value": 3,
+	            "random": true,
+	            "anim": {
+	                "speed": 6,
+	                "size_min": 0.3
+	            }
+	        },
+	        "line_linked": {
+	            "enable": false
+	        },
+	        "move": {
+	            "random": true,
+	            "speed": 3,
+	            "direction": "top",
+	            "out_mode": "out"
+	        }
+	    },
+	    "interactivity": {
+	        "events": {
+	            "onhover": {
+	                "enable": true,
+	                "mode": "bubble"
+	            },
+	            "onclick": {
+	                "enable": true,
+	                "mode": "repulse"
+	            }
+	        },
+	        "modes": {
+	            "bubble": {
+	                "distance": 250,
+	                "duration": 2,
+	                "size": 10,
+	                "opacity": 0
+	            },
+	            "repulse": {
+	                "distance": 400,
+	                "duration": 4
+	            }
+	        }
+	    }
+	}} />
+        
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkform onInputchange={this.onInputchange}  onSubmit={this.onSubmit}/>
+
+        {/* <Facerecognition/> */}
+      </div>
+    );
+  }
 }
 
 export default App;
