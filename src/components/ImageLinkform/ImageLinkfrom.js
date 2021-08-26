9a97ac1e22a1be2b7ad92c7b2553cc813bc3d2af
GuruPrasad-sr <guruprasadsr10@gmail.com>
2021-08-26 17:47:43 +0530

    Code along end of section 24

--- src/components/ImageLinkform/ImageLinkfrom.js
+++ src/components/ImageLinkform/ImageLinkfrom.js
@@ -1,19 +1,23 @@
 import React from 'react';
-import './ImageLinkform.css'
+import './ImageLinkForm.css';
 
-const ImageLinkform = ({onInputchange , onSubmit}) => {
-    return (
-        <div>
-            <p className='f3'>{'This SmartBrain will detect faces in your pictures. Give it a try'}</p>
-            <div className='center'>
-                <div className='form center pa4 br3 shadow-5 '>
-                    <input className='f4 pa2 w-70 center' type='text' placeholder='Paste url here' onChange={onInputchange} />
-                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-purple' onClick={onSubmit}>Detect</button>
-                </div>
-            </div>
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+  return (
+    <div>
+      <p className='f3'>
+        {'This Magic Brain will detect faces in your pictures. Give it a try.'}
+      </p>
+      <div className='center'>
+        <div className='form center pa4 br3 shadow-5'>
+          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+          <button
+            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+            onClick={onButtonSubmit}
+          >Detect</button>
         </div>
-
-    );
+      </div>
+    </div>
+  );
 }
 
-export default ImageLinkform;
+export default ImageLinkForm;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import brain from './brain.png';
 const Logo = () => {
     return (
         <div className='ma4 mt0 place'>
-            <Tilt className="Tilt br2 shadow-2" options={{ max: 35 }} style={{ height: 150, width: 150 }} >
+            <Tilt className="Tilt br2 shadow-2" options={{ max: 45 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner"><img style={{paddingTop:'5px'}} src={brain} alt='oops'/></div>
             </Tilt>
         </div>
